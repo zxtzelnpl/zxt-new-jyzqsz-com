63c842081ebe051a050132d2fc5b798e0649438b
was <zxt_zel_npl@163.com>
2018-07-30 13:23:54 +0800

    add investor-edu

---
+++ assets/home_new/investor_edu/blackList.js
@@ -0,0 +1,103 @@
+var blackListStrOne = '免费领取当日操盘策略http://a20.gp124.com/\t类别1-1\t2018年4月\n' +
+  '股誉天下http://zhibo.jrrow.cn/?room_s_id=stfaialxmtkzu3jz4unfrxpm\t类别1-1\t2018年4月\n' +
+  '新盈投资http://v.hxjs366.cn/  \t类别1-1\t2018年4月\n' +
+  '集金学堂http://www.yintd.com/tg/jwx05.html\t类别1-1\t2018年4月\n' +
+  '新博金融http://xbic.hk/  \t类别1-1\t2018年4月\n' +
+  '微信高端交流http://sgnew.hxtzddz.cn/\t类别1-1\t2018年3月\n' +
+  '题材挖掘机http://t01.stockhn.com/bd/YM1/index.html\t类别1-1\t2018年2月\n' +
+  '艾福瑞国际投资 http://alpari8.top/\t类别1-1\t2018年2月\n' +
+  '领好股+抢内参+学战法 https://dytz.dyrt218.com/template/html/7a/5a8e5ece9e57a.html\t类别1-1\t2018年2月\n' +
+  '顶牛贷www.a32a.com/\t类别1-1\t2017年11月\n' +
+  '财惠赚http://www.3626178.com/\t类别1-1\t2017年11月\n' +
+  '涨停牛股盘前免费送http://vvip.hanchengtz.org/p/jgztng/\t类别1-1\t2017年11月\n' +
+  '连兴投资1http://www.lxtz887.com/\t类别1-1\t2017年11月\n' +
+  '仿冒“广发证券股份有限公司” 网站：http://www.516071.com/\t类别1-3\t2017年10月\n' +
+  '官方加微信领牛股 http://win.16268.com/wxtg/h5/index.html?channel=baidutg\t类别1-1\t2017年8月\n' +
+  '进源国际 http://www.rtnbm.com/?good\t类别1-1\t2017年8月\n' +
+  '仿冒“广发证券股份有限公司” 网站：http://sh.qiyexinyong.org/corp-310000000096468.html\t类别1-3\t2017年8月\n' +
+  '仿冒“广发证券股份有限公司” 网站：http://u47882401.b2bname.com/\t类别1-3\t2017年8月\n' +
+  '仿冒“华泰证券股份有限公司” 网站：http://gxw1818.com/\t类别1-3\t2017年7月\n' +
+  '仿冒“国泰君安证券股份有限公司” http://5682732.com/\t类别1-3\t2017年7月\n' +
+  '仿冒“银河源汇投资有限公司”网站：http://www.yuanhuitz.com/\t类别1-3\t2017年7月\n' +
+  '仿冒“兴业证券股份有限公司”网站：http://www.9975544.com/\t类别1-3\t2017年7月\n' +
+  '仿冒“中银国际证券股份有限公司”网站：http://www.zy2688.com/\t类别1-3\t2017年6月\n' +
+  '仿冒“招商证券股份有限公司”网站：http://gu.99guba.com/\t类别1-3\t2017年6月\n' +
+  '牛股日报http://www.stock333.com/vip_stock333.html\t类别1-1\t2017年6月\n' +
+  '友能投资 http://www.yntz11.com/\t类别1-1\t2017年6月\n' +
+  '冒用“太平洋证券股份有限公司”的名称和标识，误导投资者，进行非法宣传和外汇交易活动：http://ygtpyzjkf.com/\t类别1-3\t2017年6月\n' +
+  '仿冒“太平洋证券股份有限公司”网站：http://www.pacific-securties.com\t类别1-3\t2017年6月\n' +
+  '仿冒“财达证券有限责任公司” 网站：http://www.s10000.cc/\t类别1-3\t2017年6月\n' +
+  '万利国际http://cjtz.hanchengtz.red/\t类别1-1\t2017年5月\n' +
+  '仿冒“兴业证券股份有限公司”网站，冒充兴业投资的名义进行非法活动的网站：http://www.xingyetouzi.com/chinese/index.html\t类别1-3\t2017年5月\n' +
+  '仿冒“方正证券股份有限公司” 湘潭板塘营业部网站http://a24126947.atobo.com.cn/\t类别1-3\t2017年4月\n' +
+  '仿冒“方正证券股份有限公司”湘潭营业部”网站http://1572890.1024sj.com/；\t类别1-3\t2017年4月\n' +
+  '每日盘前涨停牛股http://www.shy888.com/baidu/zpc/8\t类别1-1\t2017年4月\n' +
+  '仿冒“华林证券有限责任公司海口滨海大道证券营业部”网站：http://a1777130.atobo.com.cn/\t类别1-3\t2016年12月\n' +
+  '恒大投资http://www.hd5677.com/\t类别1-1\t2016年12月\n' +
+  '股预言帝http://jr.emoney.cn/hd/sgpc1/index.html\t类别1-1\t2016年9月\n' +
+  '股票开户万一起http://www.ht0170324005.icoc.me/\t类别1-1\t2016年8月\n' +
+  '每天送二只内幕消息股http://mp.weixin.qq.com/s\t类别1-1\t2016年7月\n' +
+  '淐盛T+0 http://www.662003.com/\t类别1-1\t2016年7月\n' +
+  '股市暴跌http://www.huangjintd.com/gptp/hswy.html\t类别1-1\t2016年7月\n' +
+  '个股免费义诊http://www.cjs.com.cn/bd/20141029/bd02/default.html\t类别1-5\t2016年6月\n' +
+  '“分析家公式网”网站发布非法证券活动信息http://www.88gs.com/\t类别1-1\t2013年10月';
+var blackListStrTwo = '用户5098669509的博客http://blog.sina.com.cn/5098669509\t类别1-5\t2018年4月\n' +
+  '狙击涨停板的博客http://blog.sina.com.cn/u/6524062149\t类别1-5\t2018年4月\n' +
+  '佐罗_厚恩投资-博客的博客http://blog.sina.com.cn/u/5079819925\t类别1-5\t2018年4月\n' +
+  '龙头股--涨停板的博客http://blog.sina.com.cn/mof213\t类别1-5\t2018年4月\n' +
+  '张_驰的博客http://blog.sina.com.cn/u/5998975460\t类别1-5\t2018年4月\n' +
+  '狙击涨停板的博客http://blog.sina.com.cn/u/6524062149\t类别1-5\t2018年3月\n' +
+  '奶茶糖的博客 http://blog.sina.com.cn/u/6015939839\t类别1-5\t2018年3月\n' +
+  '宫殇夏的博客http://blog.sina.com.cn/u/6016256087\t类别1-5\t2018年3月\n' +
+  '叶老师送来两Gu必建仓的博客  http://blog.sina.com.cn/u/6015935678\t类别1-5\t2018年3月\n' +
+  '淘V氣V天T尊的博客 http://blog.sina.com.cn/u/6079242521\t类别1-5\t2018年3月\n' +
+  '缠V中V说V禅的博客 http://blog.sina.com.cn/u/6079242511\t类别1-5\t2018年3月\n' +
+  '凯T恩T斯60的博客 http://blog.sina.com.cn/u/6074665860\t类别1-5\t2018年3月\n' +
+  '淘氣天尊70的博客http://blog.sina.com.cn/u/6074665870\t类别1-5\t2018年3月\n' +
+  '空v空v道v人的博客http://blog.sina.com.cn/u/6518451355\t类别1-5\t2018年3月\n' +
+  '沙v黾v农v大v盘分析的博客 http://blog.sina.com.cn/u/6518451327\t类别1-5\t2018年3月\n' +
+  '用户5098669509的博客的博客 http://blog.sina.com.cn/u/5098669509\t类别1-5\t2018年1月\n' +
+  '股市一哥的博客 http://blog.sina.com.cn/u/6234076835\t类别1-5\t2018年1月\n' +
+  '每日短线牛股交易的博客http://blog.sina.com.cn/u/3926995527\t类别1-5\t2018年1月\n' +
+  '涨停板买入法的博客http://blog.sina.com.cn/u/5096907524\t类别1-5\t2018年1月\n' +
+  '吉三牛的博客 http://blog.sina.com.cn/s/blog_17b7a88a00102xqj7.html\t类别1-5\t2017年11月\n' +
+  '羊年金股的博客http://blog.eastmoney.com/m6224094211317642/blog_258327084.html\t类别1-5\t2016年2月\n' +
+  '短线出击2016  http://blog.eastmoney.com/m9887094476417098/blog_258386948.html\t类别1-5\t2016年2月\n' +
+  '涨停金股的博客http://fdsfgjk2.blog.sohu.com/\t类别1-5\t2015年12月\n' +
+  '第1金股-涨停内参的博客http://blog.eastmoney.com/dyjg666\t类别1-5\t2015年11月\n' +
+  '拉升涨停股 最新:专家推荐3只业绩暴增的先锋股http://mszq2.blog.sohu.com/\t类别1-5\t2015年5月\n' +
+  '主力追踪股，提前布局稳赚，交流：914587789http://blog.eastmoney.com/kui9\t类别1-5\t2015年5月\n' +
+  '掘金创富 飙涨金股http://bmwx69998.blog.sohu.com/\t类别1-5\t2015年5月\n' +
+  '上海阳光私募抓涨停http://zhangtinggu88.blog.163.com/ 客服 QQ：531045456\t类别1-5\t2015年1月\n' +
+  '今日热点：明日3只必涨牛股已公开发布非法证券信息： 上海民生证券投资有限公司  http://3824382.blog.sohu.com/\t类别1-5\t2015年1月\n' +
+  '短线V直通车 http://blog.eastmoney.com/hui12zhong在线咨询QQ:19066706\t类别1-5\t2014年9月\n' +
+  '联合私募 http://adfsgdhjhk.blog.163.com/客服QQ:249834937\t类别1-5\t2014年9月\n' +
+  '反弹股糜币http://blog.eastmoney.com/blqk134QQ：369926067\t类别1-5\t2014年5月\n' +
+  '假冒“海通证券股份有限公司”博客：“海通私募” http://htzqsm.blog.163.com/QQ:185578277\t类别1-3\t2014年4月\n' +
+  '上海航空证券 http://hkff88.blog.163.com/\t类别1-4\t2014年2月\n' +
+  '股市亮剑 黑马奔腾http://gshmbt.blog.163.com/QQ:18321955878\t类别1-5\t2014年2月\n' +
+  '陆劲松解盘请加Q1770201258http://lujingsong.blog.163.com/\t类别1-5\t2013年9月\n' +
+  '天天盈利者http://blog.eastmoney.com/saadaasQQ:194219996\t类别1-5\t2013年8月\n' +
+  '创幻财经http://www.chcj.net/thread-598482-1-1.html\t类别1-5\t2013年3月'
+var blackListOne = [];
+var blackListTwo = [];
+
+function makeBlackListToBlackArray(str,list){
+  var array = str.split('\n');
+
+  array.forEach(function(item){
+    var data = item.split('\t');
+    list.push({
+      text:data[0]/*.replace('http','<br/>http')*/,
+      type:data[1],
+      time:data[2]
+    })
+  });
+}
+
+
+makeBlackListToBlackArray(blackListStrOne,blackListOne);
+makeBlackListToBlackArray(blackListStrTwo,blackListTwo);
+
+
+
